99e12416fe81bdaa8def0fb926021be674fc2d9e
Anu DAMBALADENIYA <anuradha.dambaladeniya@gmail.com>
2020-02-18 13:01:09 +1030

    Update connection.js
    Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 var connection;
-var connection = mysql.createConnection({
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+ connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "anu12358",
     database: "burgers_db"
-});
-
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
     if(err) {
